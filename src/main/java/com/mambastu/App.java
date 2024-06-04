4346d7a00a5b985f2d96495927b2ec0458665a1b
Li Haoyuan <1513624626@qq.com>
2024-06-04 18:01:18 +0800

    docs: modify gitIgnore

--- src/main/java/com/mambastu/App.java
+++ src/main/java/com/mambastu/App.java
@@ -1,8 +1,6 @@
 package com.mambastu;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -17,18 +15,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
--- target/classes/com/mambastu/expo/menu/README.md
+++
@@ -1,1 +0,0 @@
-## Main Menu and Level Menu
