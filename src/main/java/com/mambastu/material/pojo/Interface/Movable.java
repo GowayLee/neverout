b0b8e6b5b91651c42352ad551953cc66b3f5dc50
梁梓茂 <zimaoliang89@gmail.com>
2024-06-12 17:55:08 +0800

    feat: 补充提交

---
+++ src/main/java/com/mambastu/material/pojo/Interface/Movable.java
@@ -0,0 +1,6 @@
+package com.mambastu.material.pojo.entity.Interface;
+
+public interface Movable {
+    public void move();
+
+}
---
+++ src/main/java/com/mambastu/material/pojo/enums/CollisionState.java
@@ -0,0 +1,6 @@
+package com.mambastu.material.pojo.entity.enums;
+
+public enum CollisionState {
+    VERTICAL,HORIZONTAL,BOTH,
+    TRUE,FALSE
+}
